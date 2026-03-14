9fe40dd0fcf72a6011a15e7bdb1a9b0fd72bd2c9
Andres Bravo <andbrav1917@gmail.com>
2026-03-14 15:36:45 -0400

    refactor: import submitButtonCreateTask corrected

--- src/components/tasks/createTask.tsx
+++ src/components/tasks/createTask.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { SubmitButtonCreateTask } from '@/components/tasks/submit-buttonTask';
+import { SubmitButtonCreateTask } from '@/components/tasks/Submit-buttonTask';
 import { useActionState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
